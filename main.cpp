be78213c4f5154ca8922ca99a5ba5b3d12dfb5b6
MaksimMyson <maksimmyson33@gmail.com>
2023-10-22 13:30:10 +0300

    ex2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-using namespace std;
 
-int main(){
+int main() {
+    double num1, num2;
 
-	return 0;
-		
+    std::cout << "Enter number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter second number: ";
+    std::cin >> num2;
+
+    if (num1 < num2) {
+        std::cout << "Smaller number: " << num1 << std::endl;
+    }
+    else {
+        std::cout << "Smaller number: " << num2 << std::endl;
+    }
+
+    return 0;
 }
