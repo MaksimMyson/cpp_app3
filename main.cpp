ba77760f8e6d8c9128da1b128ee28dfa395f0ce4
MaksimMyson <maksimmyson33@gmail.com>
2023-10-22 13:32:39 +0300

    ex3

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
-
-int main(){
-
+int main()
+{
+	int(a);
+		cout << "Enter a number: ";
+	cin >> a;
+	if (a > 0)
+		cout << "The number is positive";
+	else if (a < 0)
+		cout << "The number is negative";
+	else
+		cout << "The number is zero";
 	return 0;
-		
 }
