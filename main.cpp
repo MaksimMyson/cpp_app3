bf53d096bbf272f252c5476c1d4172904730e676
MaksimMyson <maksimmyson33@gmail.com>
2023-10-22 13:34:33 +0300

    ex4

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
-using namespace std;
 
-int main(){
+int main() {
+    int a, b;
 
-	return 0;
-		
+    std::cout << "Enter number: ";
+    std::cin >> a;
+
+    std::cout << "Enter second number: ";
+    std::cin >> b;
+
+    if (a == b) {
+        std::cout << "Enter a even numbers." << std::endl;
+    }
+    else {
+        std::cout << "Entered numbers are not equal." << std::endl;
+
+        if (a < b) {
+            std::cout << "Numbers in ascending order: " << a << ", " << b << std::endl;
+        }
+        else {
+            std::cout << "Numbers in ascending order: " << b << ", " << a << std::endl;
+        }
+    }
+
+    return 0;
 }
